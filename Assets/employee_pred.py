370738b6cd53f0724327fd08757cda8c2efa5618
Livesh kumar <57588397+dev-osiris@users.noreply.github.com>
2023-05-09 20:23:32 +0530

    changed pkl path

--- Assets/employee_pred.py
+++ Assets/employee_pred.py
@@ -2,11 +2,13 @@
 import pickle
 
 # de-serialize model.pkl file into an object called mdl using pickle
-with open('Assets\pickle\model.pkl', 'rb') as handle:
+# with open('Assets\pickle\model.pkl', 'rb') as handle:
+with open('/opt/render/project/src/Assets/pickle/model.pkl', 'rb') as handle:
     mdl = pickle.load(handle)
 
 # de-serialize scalar.pkl file into an object called scl using pickle
-with open('Assets\pickle\scalar.pkl', 'rb') as handle:
+# with open('Assets\pickle\scalar.pkl', 'rb') as handle:
+with open('/opt/render/project/src/Assets/scalar.pkl', 'rb') as handle:
     scl = pickle.load(handle)
 
 # scale the user fed data
